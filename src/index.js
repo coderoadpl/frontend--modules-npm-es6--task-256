5f2b0ea9bca40ca73cfe6ac1ab5ddbe102863b29
Mateusz Choma <kontakt@coderoad.pl>
2020-09-22 08:20:52 +0200

    Optional chaining

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const me = {
+    name: 'Mateusz',
+    car: {
+        brand: 'Opel',
+    },
+}
+
+console.log(me.name)
+console.log(me.lastName)
+
+console.log(me.car)
+console.log(me.car.brand)
+console.log(me.car.model)
+
+console.log(me.boat)
+console.log(me.boat && me.boat.brand)
+console.log(me.boat?.brand)
